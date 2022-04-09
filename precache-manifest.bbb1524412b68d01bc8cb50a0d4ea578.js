e4b77bbc4dd6bc5d402e0e308ae5023fc15a9cec
AhmerAli412 <aliahmer289@gmail.com>
2022-04-09 18:17:26 +0500

    Updates

---
+++ precache-manifest.bbb1524412b68d01bc8cb50a0d4ea578.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c3612d8abcf49860b19",
+    "url": "/Gym/static/css/main.3126e64c.chunk.css"
+  },
+  {
+    "revision": "3c3612d8abcf49860b19",
+    "url": "/Gym/static/js/main.3c3612d8.chunk.js"
+  },
+  {
+    "revision": "aa1ef92c5cf7728f9d0a",
+    "url": "/Gym/static/js/1.aa1ef92c.chunk.js"
+  },
+  {
+    "revision": "7b89fb21947c69cdcfbf",
+    "url": "/Gym/static/js/2.7b89fb21.chunk.js"
+  },
+  {
+    "revision": "aac25fc1fed30ec10abc",
+    "url": "/Gym/static/js/runtime~main.aac25fc1.js"
+  },
+  {
+    "revision": "9e6eeb0393fd362f0f1483ab17ed75f3",
+    "url": "/Gym/static/media/banner.9e6eeb03.png"
+  },
+  {
+    "revision": "43ea303526f93a3a36da30b6f6db7cbf",
+    "url": "/Gym/static/media/offer.43ea3035.png"
+  },
+  {
+    "revision": "9b99e1877ea384fe9de27fe784da4afe",
+    "url": "/Gym/static/media/1.9b99e187.svg"
+  },
+  {
+    "revision": "a609d5e7c4149ca1e4c342521453d3c1",
+    "url": "/Gym/static/media/2.a609d5e7.svg"
+  },
+  {
+    "revision": "f93ab23617fd630eae3f7f70871ccd90",
+    "url": "/Gym/static/media/3.f93ab236.svg"
+  },
+  {
+    "revision": "83e019f14222ea38febf9ee965e40916",
+    "url": "/Gym/static/media/4.83e019f1.svg"
+  },
+  {
+    "revision": "839e7fc9759a45dd6b3e9faa59c42519",
+    "url": "/Gym/static/media/about.839e7fc9.png"
+  },
+  {
+    "revision": "7aec6ab147ae78da20423a6e88fd5b75",
+    "url": "/Gym/index.html"
+  }
+];
